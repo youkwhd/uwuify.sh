db32ec3e902f2b18f8f91a115730d7d0a8b22280
youkwhd <lolywk@tutanota.com>
2023-07-08 17:36:15 +0700

    feat: replace [lr] with w using RegExp

--- uwuify.sh
+++ uwuify.sh
@@ -1,1 +1,15 @@
 #!/bin/sh
+
+SCRIPT_NAME=$(basename $0)
+
+function help() {
+    echo "usage: $SCRIPT_NAME <text>"
+}
+
+if [[ $# -eq 0 ]] ; then
+    help
+    >&2 echo "$SCRIPT_NAME: no argument provided"
+    exit 1
+fi
+
+echo $(echo $1 | sed -e "s/[lr]/w/g" | sed -e "s/[LR]/W/g")
